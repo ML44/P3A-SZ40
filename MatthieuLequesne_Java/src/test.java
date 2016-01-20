730e80f0aaf4d6dc96a57d44773e97fd65d610ba
MatthieuLequesne <matthieulequesne@gmail.com>
2016-01-20 16:00:33 +0100

    Motifs entrees sorties

--- MatthieuLequesne_Java/src/test.java
+++ MatthieuLequesne_Java/src/test.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,30 +10,33 @@ import java.nio.file.Paths;
 public class test {
 
 	public static void main(String[] args) throws IOException {
+				
 		
-		int n = 2;
+		// Lecture du fichier et de la clé
+		String s = args[0];
 		
-		// Lecture du fichier chiffré et de la clé
-		String chiffre = new String(Files.readAllBytes(Paths.get("data/challenge"+n+".txt")));
-		Lorentz L = new Lorentz("data/key"+n+".txt");
+		Normalisation.normalise(s);
+		Lorentz L = new Lorentz("data/"+s+".cle");
 
-		String clair = "";
+		FileInputStream in = new FileInputStream("data/"+s+".norm");
+		FileOutputStream out = new FileOutputStream("data/"+s+"_out.txt");
+
+		File p = new File ("data/"+s+".process");
+		FileWriter fw = new FileWriter (p);		
+		fw.write("Step \tInput \tOutput\tK1 \tK2 \tK3 \tK4 \tK5 \tM1 \tM2 \tS1 \tS2 \tS3 \tS4 \tS5 \tChi \tPsi \tM1 \tM2 \tTM \n");
 		Baudot b;
-		File p = new File ("data/process"+n+".txt");
-		FileWriter fwp = new FileWriter (p);		
-		fwp.write("Step \tInput \tOutput \tK1 \tK2 \tK3 \tK4 \tK5 \tM1 \tM2 \tS1 \tS2 \tS3 \tS4 \tS5 \tChi \tPsi \tM1 \tM2 \tTM \n");
-
-		for(int i=0;i<chiffre.length();i++)
-		{
-			b = new Baudot(chiffre.charAt(i));
-			clair += L.encrypt(b, fwp).getChar();
+		int k=0;
+		char c;
+		while(k!=-1){
+			k = in.read();
+			c = (char) k;
+			b = new Baudot(c);
+			out.write(L.encrypt(b, fw).getChar());
 		}
 		
-		File f = new File ("data/clair"+n+".txt");
-		FileWriter fw = new FileWriter (f);
-		fw.write (clair);
+		in.close();
+		out.close();
 	    fw.close();
-	    fwp.close();
 
 	}
 
